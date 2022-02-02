8053457d94b5235afb42f9536cc717338a16cfd5
iGalaxyYT <contact@igalaxy.dev>
2022-02-01 20:58:30 -0800

    fix: make padding consistent on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,7 @@ export default function Home(props: Props) {
 		useGitHubPinnedRepos(GITHUB_USERNAME);
 
 	return (
-		<div style={{ paddingBottom: '160px' }}>
+		<div>
 			<div style={{ marginBottom: '18px' }}>
 				{socials.map((social, i) => (
 					<Link href={social.link} passHref key={`social${i}`}>
